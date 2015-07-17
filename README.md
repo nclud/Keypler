43a4a1df1cdef0e6f0749641b41bf875e7a8809d
Matthew Kaufer <mjkaufer@gmail.com>
2015-07-17 15:55:05 -0400

    Configured routes, added todo

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Keypler
 
 Automated API & License key generation, for Meteor!
+
+## Todo
+
+* Configuration to use SendGrid's webhooks or GumRoad's
+* Let user customize authentication key's charset & length
--- keypler.js
+++ keypler.js
@@ -1,1 +1,20 @@
-// Write your package code here!
+if(Meteor.isServer){
+
+	_Keypler_KeyCollection = new Meteor.Collection('_keypler_keyCollection');
+
+	Router.route('/keypler_webhook', function () {
+		var req = this.request;
+		var res = this.response;
+		
+		console.log(req)
+		console.log(req.method)//GET OR POST
+		console.log(req.body)
+
+		//update collection
+		//https://sendgrid.com/docs/API_Reference/Webhooks/parse.html
+
+		res.end('hello from the server\n');
+	}, {
+		where: 'server'
+	});	
+}
