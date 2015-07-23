20f43d49bb288373d2e8d898e50def0200077d65
Matthew Kaufer <mjkaufer@gmail.com>
2015-07-23 11:19:11 -0400

    Configuration option for route, ability to verify by id or email, updated readme

--- README.md
+++ README.md
@@ -19,9 +19,27 @@ The following table explains the fields, their defaults, and what they do.
 | `publish`     | `boolean`  | true                                             | Defines whether or not Keypler will publish a user's license to the client side. This means users can see their license by looking at `Meteor.user().services.keypler.license`
 | `makeLicense` | `function` | SHA or GUID function, depending on `licenseType` | A function which takes an argument of `userId` and returns a license key.
 | `licenseType` | `String`   | `"sha"`                                          | The type of license to return. **Only to be defined if you do *not* define your own `makeLicense` function.** Options are `sha` and `guid`. Note: The `guid` function generated a random GUID. The `sha` function generates a SHA-1 hash based off of the userId salted with `Math.random() * Date.now()`
+| `makeRoute`   | `boolean`  | true                                             | If `makeRoute` is set to true, Keypler will create a serverside route called `/keypler_verify` which can verify licenses
 
+Keypler will automatically give each new user a `services.keypler.license` field, which has `license` set to `null` by default.
 
-Keypler will automatically give each new user a `services.keypler` field, which has `license` set to `null` by default.
+## Authentication
+
+Keypler uses Iron Router to create a server route called `/keypler_verify` where external applications can authenticate. This route is created by default, but you can disable it by setting `makeRoute` to false in the configuration object.
+
+To authenticate a user, send a `POST` request to `http://your.meteor.app/keypler_verify`. The `POST` body should contain the following JSON
+
+```JSON
+{
+	email: userEmail,
+	id: userId,
+	license: userLicense
+}
+```
+
+The `license` value is mandatory, and you can submit *either* the user's email in the `email` field, or the user's `_id` in the `id` field (No underscore in the `POST` body). 
+
+The route will return a status code of `202` if the user has the license specified in the body of the request. Otherwise, a `401` is returned.
 
 ## Todo
 
--- keypler.js
+++ keypler.js
@@ -9,7 +9,8 @@ if(Meteor.isServer){
 			makeLicense: function(userId){
 				//returns a license based off of a userId, default returns a SHA of the id plus the timestamp multiplied by math.random
 			},
-			licenseType: "sha" | "guid"//only to be set if makeLicense is not defined - as a note, the guid makeLicense function does not actually use the userId
+			licenseType: "sha" | "guid"//only to be set if makeLicense is not defined - as a note, the guid makeLicense function does not actually use the userId,
+			makeRoute: true//whether or not a route is created
 		}
 
 		*/
@@ -35,22 +36,15 @@ if(Meteor.isServer){
 		}
 
 
-		this.generateLicense = function(userId){
+		this.generateLicense = function(userId){//Keypler function to generate a license - only available serverside, like the rest of Keypler
 
 			var user = Meteor.users.findOne({_id: userId})
 
 			if(user && user.services && user.services.keypler && user.services.keypler.license)
-				return false;
+				return false;//returns false if the user has a license
 
-			Meteor.users.update({_id: userId}, {$set:{'services.keypler.license': this.makeLicense(userId)}})
-
-
-			return Meteor.users.findOne({_id: userId}, {
-				fields: {
-					_id: 1,
-					'services.keypler.license': 1
-				}
-			})
+			return Meteor.users.update({_id: userId}, {$set:{'services.keypler.license': this.makeLicense(userId)}})
+			//returns the id of the user who received a license
 		}
 
 		if(this.publish === undefined || this.publish)
@@ -63,13 +57,13 @@ if(Meteor.isServer){
 				})
 			})
 
-		Accounts.onCreateUser(function(options, user){
+		Accounts.onCreateUser(function(options, user){//give all new users an empty license
 
 			if(!user.services)
 				user.services = {};
 
 			user.services.keypler = {};
-			user.services.keypler.license = "";
+			user.services.keypler.license = null;
 
 			if(options.profile)
 				user.profile = options.profile;
@@ -77,106 +71,63 @@ if(Meteor.isServer){
 			return user;
 		})
 
+		if(this.makeRoute === undefined || this.makeRoute)
+			Router.route('/keypler_verify', function(){
+				/*
+					POST request with JSON body
+					key `email` has user's email
+					key `license` has license
+					response: 
+						202 if the key is verified
+						400 if it's not a POST request
+						401 if the key doesn't match
 
-		Router.route('/keypler_verify', function(){
-			/*
-				POST request with JSON body
-				key `email` has user's email
-				key `license` has license
-				response: 
-					202 if the key is verified
-					400 if it's not a POST request
-					401 if the key doesn't match
+				*/
 
-			*/
+				var req = this.request;
+				var res = this.response;
 
-			var req = this.request;
-			var res = this.response;
-			
 
-			if(req.method !== "POST"){
-				res.writeHead(400, {'Content-Type': 'text/plain'});
-				res.write("Bad request");
-				return res.end();
-			}
+				var query = {};
 
+				if(req.body.email)
+					query['emails.0.address'] = req.body.email
 				
-			// res.status(200).send('A+').end();
-			var userQuery = Meteor.users.findOne({'emails.0.address': req.body.email, 'services.keypler.license': req.body.license});
-			if(userQuery && userQuery.services && userQuery.services.keypler && userQuery.services.keypler.license){//they have a license
-				res.writeHead(202, {'Content-Type': 'text/plain'});
-				res.write("Everything checks out, boss");
+				if(req.body.id)
+					query['_id'] = req.body.id
+
+
+				if(Object.keys(query).length == 0 || req.method !== "POST"){
+					res.writeHead(400, {'Content-Type': 'text/plain'});
+					res.write("Bad request");
+					return res.end();
+				}
+
+				query['services.keypler.license'] = req.body.license
+
+				// res.status(200).send('A+').end();
+				var userQuery = Meteor.users.findOne(query);
+
+				if(userQuery && userQuery.services && userQuery.services.keypler && userQuery.services.keypler.license){//they have a license
+					res.writeHead(202, {'Content-Type': 'text/plain'});
+					res.write("Everything checks out, boss");
+					
+					return res.end();
+				}
 				
-				return res.end();
-			}
-			
 
-			res.writeHead(401, {'Content-Type': 'text/plain'});
-			res.write("This user's key is invalid!");
-			res.end();
+				res.writeHead(401, {'Content-Type': 'text/plain'});
+				res.write("This user's key is invalid!");
+				res.end();
 
-		},
-		{where: 'server'})	
+			},
+			{where: 'server'})	
 
-	};
-
-
-
-	// Keypler.HMACKey = "RazzleFrazzle";
-
-	// Keypler.setHMACKey = function(key){
-	// 	Keypler.HMACKey = key;
-	// }
-
-	/*
-	_Keypler_KeyCollection = new Meteor.Collection('_keypler_keyCollection');
-
-	Router.route('/keypler_webhook', function () {
-		var req = this.request;
-		var res = this.response;
-
-		console.log(req)
-		console.log(req.method)//GET OR POST
-		console.log(req.body)
-
-
-		//Psuedocode to be implemented when more details are figured out
-
-		// ALLOWED_ADDRESSES = ["allowedEmail@sendgrid.com", "test@test.test"]
-
-		// if(ALLOWED_ADDRESSES.indexOf(req.body.from) == -1)//so people can't spoof emails
-		// 	return;
-
-		// var userEmail = ....//some field
-
-		// if(_Keypler_KeyCollection.findOne({userEmail: userEmail}))//user already has an auth key
-		// 	return;
-
-		// _Keypler_KeyCollection.insert({userEmail: userEmail})
-
-
-		//get user's name
-		//update collection
-		//https://sendgrid.com/docs/API_Reference/Webhooks/parse.html
-
-		
-
-		
-
-
-
-		res.end('hello from the server\n');
-	}, {
-		where: 'server'
-	});
-	*/
-
-
-
+		};
 
 
 }
 
 if(Meteor.isClient){
-	Meteor.subscribe('_keypler_userKey')
+	Meteor.subscribe('_keypler_userKey')//automatically subscribe to this - only publish the actual data if configured
 }
